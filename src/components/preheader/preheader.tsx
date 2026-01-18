4edd65a9c6b024f82ef0f74d9d249e2676546492
Vadim_i4 <vadpon5@gmail.com>
2026-01-18 19:48:43 +0200

    feat: icon vk

---
+++ src/components/preheader/preheader.tsx
@@ -0,0 +1,15 @@
+import { Box, Stack } from "@chakra-ui/react"
+import { FaVk } from "react-icons/fa6";
+
+const Preheader = () => {
+  return (
+    <Stack direction="row" h="20" >
+      <Box>
+        <FaVk />
+      </Box>
+
+    </Stack>
+  )
+}
+
+export default Preheader
